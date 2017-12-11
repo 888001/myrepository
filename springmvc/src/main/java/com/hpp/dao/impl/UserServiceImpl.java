d288f486220fb3163a5d47e99b42f5dcf0ffad97
胡鹏鹏 <1171132590@qq.com>
2017-12-11 15:19:37 +0800

    删除TODO 1.mybatis普通dao模式开发,首先xml命名空间可以自定义,随后dao,daoImpl构造方法注入sqlsession,sqlSession.selectOne("test.findUserById", id); 2.mybatis接口代理开发模式,xml名称空间名字需要制定接口名称,方法名称和xml的id相同,sqlSession.getMapper(UserMapper.class)即可;

--- springmvc/src/main/java/com/hpp/dao/impl/UserServiceImpl.java
+++ springmvc/src/main/java/com/hpp/dao/impl/UserServiceImpl.java
@@ -13,7 +13,6 @@ public class UserServiceImpl implements UserService {
 	UserTMapper userTMapper;
 
 	public UserT selectAllUser() {
-		// TODO Auto-generated method stub
 		return userTMapper.selectAllUser();
 	}
 
